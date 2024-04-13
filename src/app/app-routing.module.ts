0d105b96fff49880bbbea64ab1d9aa662d30b956
Iulia Ilies <iulia.ilies01@gmail.com>
2024-04-13 19:18:40 +0300

    Add nav footer and choose profile dropdown

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'trainings',
     loadChildren: () => import('./trainings/trainings.module').then(m => m.TrainingsModule),
   },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule),
+  },
 ]
 
 
--- src/app/core/nav/nav.component.ts
+++ src/app/core/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 import { PATHS } from '../../app.constants';
 import { AuthService } from '../../auth/shared/auth.service';
@@ -11,9 +11,29 @@ import { AuthService } from '../../auth/shared/auth.service';
 export class NavComponent {
   PATHS = PATHS;
 
+  userDropdownOpened = false;
+
   constructor(
+    private elementRef: ElementRef,
     public auth: AuthService,
     public router: Router,
   ) {}
 
+  toggleUserDropdown(): void {
+    this.userDropdownOpened = !this.userDropdownOpened;
+  }
+
+  logout(): void {
+
+  }
+
+  @HostListener('document:click', ['$event']) onDropdownBlur(e: MouseEvent): void {
+    if (this.userDropdownOpened) {
+      const userItem: HTMLElement = this.elementRef.nativeElement.querySelector('.user-icon');
+      if (e.target === userItem || userItem.contains(e.target as Node)) {
+        return;
+      }
+      this.userDropdownOpened = false;
+    }
+  }
 }
